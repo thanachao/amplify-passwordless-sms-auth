41fb91e93fb55ccd987d157df106786d56b1240d
Chao Luengwitayakorn <thanachao@gmail.com>
2023-06-19 14:46:13 -0300

    Update environment.prod.ts

--- webClient/angular/src/environments/environment.prod.ts
+++ webClient/angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  region: 'your region',
-  userPoolId: 'your user pool id',
-  userPoolWebClientId: 'your client id',
+  region: 'us-east-1',
+  userPoolId: 'us-east-1_3dDW4oeNB',
+  userPoolWebClientId: '63ttklq655sa1l7vj77ee3idkq',
 };
